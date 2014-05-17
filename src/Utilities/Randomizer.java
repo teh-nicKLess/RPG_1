22dc0d7bfac4575994a2f809fe39aafdf2ee504a
Netsky <Netsky@Netsky-HPC>
2014-05-17 13:59:59 +0200

    added getRandomNumberNIncluded, with returns an int between [0, n] added documentation

--- src/Utilities/Randomizer.java
+++ src/Utilities/Randomizer.java
@@ -5,17 +5,40 @@ import java.util.Random;
 
 public abstract class Randomizer {
     
+    //Holds the Random object
     private final static Random rnd = new Random();
     
     
+    /**
+     * returns an Integer [0, n]
+     * @param n upper bound
+     * @return random Integer [0, n]
+     */
+    public static int getRandomNumberNIncluded(final int n) {
+        return getRandomNumber(n + 1);
+    }
+    
+    /**
+     * returns an Integer [0, n)
+     * @param n upper bound
+     * @return random Integer [0, n)
+     */
     public static int getRandomNumber(final int n) {
         return n <= 0 ?  0 : rnd.nextInt(n);
     }
     
+    /**
+     * returns a random int
+     * @return a random int
+     */
     public static int getRandomNumber() {
         return rnd.nextInt();
     }
     
+    /**
+     * returns a random double [-1, 1]
+     * @return a random double [-1, 1]
+     */
     public static double getNextGaussian() {
         return rnd.nextGaussian();
     }

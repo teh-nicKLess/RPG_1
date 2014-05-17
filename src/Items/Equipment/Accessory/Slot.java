dc19cdeb7dee60053b14e4cb3f2c16c999f9213d
Netsky <Netsky@Netsky-HPC>
2014-05-17 18:06:45 +0200

    added documentation

--- src/Items/Equipment/Accessory/Slot.java
+++ src/Items/Equipment/Accessory/Slot.java
@@ -10,15 +10,26 @@ public enum Slot {
     
     private final short durability = 200;
     
-    
+    /**
+     * returns the durability
+     * @return the durability
+     */
     protected final int getDurability() {
         return durability;
     }
     
+    /**
+     * returns the weight
+     * @return the weight 
+     */
     protected Weight getWeight() {
         return Weight.NULL;
     }
     
+    /**
+     * returns a random Slot object
+     * @return a random Slot object 
+     */
     public static Slot getRandomSlot() {
         return Slot.values()[Randomizer.getRandomNumber(Slot.values().length)];
     }

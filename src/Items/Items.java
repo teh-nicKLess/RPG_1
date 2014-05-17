6a0c8193ff8c8f844d6486acd95fef5ade5c634f
Netsky <Netsky@Netsky-HPC>
2014-05-17 18:34:49 +0200

    added documentation

--- src/Items/Items.java
+++ src/Items/Items.java
@@ -6,20 +6,36 @@ public abstract class Items {
     private Quality quality;
     
     
+    /**
+     * returns the item's name
+     * @return the item's name 
+     */
     public final String getName() {
         return name;
     }
     
+    /**
+     * returns the item's Quality
+     * @return the item's Quality
+     */
     public final Quality getQuality() {
         return quality;
     } 
-        
+    
+    /**
+     * sets the item's name
+     * @param name - name to set
+     */
     protected final void setName(final String name) {
         if (this.name == null && name != null && !name.trim().isEmpty()) {
             this.name = name.trim();
         } 
     }
     
+    /**
+     * sets the item's Quality
+     * @param qual - Quality to set
+     */
     protected final void setQuality(final Quality qual) {
         if (quality == null && qual != null) {
             this.quality = qual;

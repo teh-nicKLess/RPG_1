5a3aa0f02bf14a1ad342cb427c2f3f5c23ee133f
Netskeh <netsky08@gmail.com>
2014-06-13 20:44:54 +0200

    crude implementation

--- src/Character/Player.java
+++ src/Character/Player.java
@@ -4,5 +4,10 @@ package Character;
 public class Player extends Character {
 
     
+    public Player(BaseStats bs, int accessorySlots) {
+        this.setStats(bs);
+        this.setLoadout(new Loadout(this, accessorySlots));
+        this.addInventory(new Inventory());
+    }
     
 }

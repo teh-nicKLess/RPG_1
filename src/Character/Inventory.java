694422c647752f55a01c8c985f85c5f2dfee6ee8
Netskeh <netsky08@gmail.com>
2014-06-13 20:44:31 +0200

    basic implementation

--- src/Character/Inventory.java
+++ src/Character/Inventory.java
@@ -1,10 +1,37 @@
 package Character;
 
 import Items.Item;
-import java.util.ArrayList;
 
 
 public class Inventory {
     
-    private ArrayList<Item> inv;
+    private static final int defaultRows = 10;
+    private static final int defaultColumns = 10;
+    
+    private final Item[][] inv;
+    
+    
+    public Inventory() {
+        this(defaultRows, defaultColumns);
+    }
+    
+    public Inventory(int rows, int columns) {
+        inv = new Item[rows][columns];
+    }
+    
+    public Item[][] getInventory() {
+        return inv.clone();
+    }
+    
+    public Item getItem(int row, int column) {
+        return inv[row][column];
+    }
+    
+    public Item setItem(Item item, int row, int column) {
+        Item temp = inv[row][column];
+        inv[row][column] = item;
+        return temp;
+    }
+    
+    
 }

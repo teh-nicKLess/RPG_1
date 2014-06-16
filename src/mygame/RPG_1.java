fc82c2d335e80468f82865ecf71ea9f31d3b5dd9
Netskeh <netsky08@gmail.com>
2014-06-16 10:50:17 +0200

    cleaned up main

--- src/mygame/RPG_1.java
+++ src/mygame/RPG_1.java
@@ -1,15 +1,12 @@
 package mygame;
 
-import Items.Equipment.Accessory.Accessory;
-import Items.Equipment.Armour.Armour;
-import Items.Equipment.Weapon.Weapon;
 import com.jme3.app.SimpleApplication;
 import java.util.Locale;
 
 
 public class RPG_1 extends SimpleApplication {
 
-    public static Locale currentLocale;
+    public static Locale currentLocale = new Locale(System.getProperty("user.language"));;
     
     
     public static void main(String[] args) {
@@ -19,24 +16,6 @@ public class RPG_1 extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        
-        final int iterations = 5;
-        currentLocale = new Locale(System.getProperty("user.language"));
-                
-        
-        for (int i = 0; i < iterations; i++) {
-            final Armour a = new Armour();
-            //System.out.println(a);
-            System.out.println(a.getName());
-            
-            final Weapon w = new Weapon();
-            //System.out.println(w);
-            System.out.println(w.getName());
-            
-            final Accessory ac = new Accessory();
-            System.out.println(ac.getName());
-            
-        }
                 
     }
 
